56a2e7d6877980c62bf97923eff8b877bdf343b4
Steven Harradine <stevenharradine@gmail.com>
2015-09-20 15:27:27 -0400

    curl instead of redirect

--- proxy.php
+++ proxy.php
@@ -1,4 +1,16 @@
 <?php
   $new_url = $_REQUEST['url'];
-  header("Location: " . $new_url);
-  die ();
+
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $new_url,
+    CURLOPT_USERAGENT => 'SARAH Proxy'
+));
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
+ echo $resp;
